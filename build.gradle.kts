6fcee4a70ed45a11c1861e147d0e197d7e159ffe
Orkhan <oguseynov@users.noreply.github.com>
2020-10-20 23:44:15 +0200

    Save initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.github.oguseynov"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "money-formatter-be"
+
