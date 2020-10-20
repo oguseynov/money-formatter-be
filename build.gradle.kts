03f3ba23447b3d0079231eec235e4587bc5d95c8
Orkhan <oguseynov@users.noreply.github.com>
2020-10-21 00:55:11 +0200

    Add htt4k and kotest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,16 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    implementation("org.http4k:http4k-core:3.268.0")
+    implementation("org.http4k:http4k-client-okhttp:3.268.0")
+    implementation("org.http4k:http4k-server-netty:3.268.0")
+    testImplementation("org.http4k:http4k-testing-hamkrest:3.268.0")
+    testImplementation("org.http4k:http4k-testing-kotest:3.268.0")
+
+    testImplementation("io.kotest:kotest-runner-junit5:4.3.0")
+    testImplementation("io.kotest:kotest-assertions-core:4.3.0")
+    testImplementation("io.kotest:kotest-property:4.3.0")
 }
 
 tasks {
@@ -18,5 +28,9 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
     }
 }
