a3afefdc6793a2b772f64a0202deae32d0423668
Orkhan <oguseynov@users.noreply.github.com>
2020-10-22 00:08:44 +0200

    Add kotlin-result dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:4.3.0")
     testImplementation("io.kotest:kotest-assertions-core:4.3.0")
     testImplementation("io.kotest:kotest-property:4.3.0")
+
+    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.6")
 }
 
 tasks {
