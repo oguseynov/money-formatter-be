a11b4f4227cf5d53eee0335ec3c70c646f998955
Orkhan <oguseynov@users.noreply.github.com>
2020-10-22 20:31:15 +0200

    Add Local as input with default value to format function

--- src/main/kotlin/Formatter.kt
+++ src/main/kotlin/Formatter.kt
@@ -2,10 +2,11 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
+import java.util.*
 
-fun format(number: BigDecimal): String {
+fun format(number: BigDecimal, local: Locale = Locale.UK): String {
 
-    val decimalFormatSymbols = DecimalFormatSymbols()
+    val decimalFormatSymbols = DecimalFormatSymbols(local)
     decimalFormatSymbols.groupingSeparator = ' '
 
     val decimalFormat = DecimalFormat("#,###.00", decimalFormatSymbols)
